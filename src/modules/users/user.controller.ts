711c40c5ac9fb4541606f14a9133b1a94317e46b
GabrielSN4981 <gabrielsantosnogueira2005@gmail.com>
2026-05-11 23:23:11 -0300

    feat: interceptors e param decorators

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,16 +1,8 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDTO } from './domain/dto/createUser.dto';
 import { UpdateUserDTO } from './domain/dto/updateUser.dto';
+import { ParamId } from 'src/shared/decorators/paramId.decorator';
 
 @Controller('users')
 export class UserController {
@@ -22,7 +14,7 @@ export class UserController {
   }
 
   @Get(':id')
-  show(@Param('id', ParseIntPipe) id: number) {
+  show(@ParamId() id: number) {
     return this.userService.show(id);
   }
 
@@ -32,12 +24,12 @@ export class UserController {
   }
 
   @Patch(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDTO) {
+  update(@ParamId() id: number, @Body() body: UpdateUserDTO) {
     return this.userService.update(id, body);
   }
 
   @Delete(':id')
-  delete(@Param('id', ParseIntPipe) id: number) {
+  delete(@ParamId() id: number) {
     return this.userService.delete(id);
   }
 }
